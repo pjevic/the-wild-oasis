a9ba45c8bc2b18d5b6458592eb091aeff4313364
Milan Pjevic <milanpjevic@icloud.com>
2024-12-15 17:22:23 +0100

    Error Boundary

--- src/main.jsx
+++ src/main.jsx
@@ -5,11 +5,22 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
 import { suppressReactWarnings } from "./utils/supressWarnings.js";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorFallback from "./ui/ErrorFallback.jsx";
+import GlobalStyles from "./styles/GlobalStyles.js";
 
 suppressReactWarnings();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <>
+    <GlobalStyles />
+    <React.StrictMode>
+      <ErrorBoundary
+        FallbackComponent={ErrorFallback}
+        onReset={() => window.location.replace("/")}
+      >
+        <App />
+      </ErrorBoundary>
+    </React.StrictMode>
+  </>
 );
